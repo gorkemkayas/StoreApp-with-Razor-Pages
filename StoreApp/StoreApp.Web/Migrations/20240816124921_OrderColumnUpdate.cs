4203378c54bbb3943cfc715b6686b410c84b94b7
= <gorkemkayas@hotmail.com>
2024-08-19 18:54:47 +0300

    StoreApp with Razor Pages

---
+++ StoreApp/StoreApp.Web/Migrations/20240816124921_OrderColumnUpdate.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StoreApp.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class OrderColumnUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Orders",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Orders");
+        }
+    }
+}
